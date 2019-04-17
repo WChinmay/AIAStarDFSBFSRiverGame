0251ddad3a6cc9d80eb6be9b7475f827623be5fd
Chinmay <wadgaonc@oregonstate.edu>
2019-04-16 19:43:40 -0700

    Added text files for states. Modified graph def.

--- BFS, IDFS, A star search/assignment1.py
+++ BFS, IDFS, A star search/assignment1.py
@@ -1,13 +1,12 @@
 import pdb
 
-
-leftbank = []
-rightbank = []
-
+# Graph Definition
 class Node:
-    def __init__(self,val, child = []):     # Node("",[Node(4),Node(5)])
+    def __init__(self,val, child = [], leftbank = [], rightbank = []):     # Node("",[Node(4),Node(5)])
         self.val = val
         self.child = child
+        self.leftbank = []      # C, W, B
+        self.rightbank = []     # C, W, B
 
 class Graph:
     def __init__(self, val, children):
@@ -18,7 +17,7 @@ class Graph:
         for node in nodes:
             self.state.child.append(Node(node))
 
-x  = Graph(3,[Node(5,[Node(3)])])       # Syntax similar to functional programming
+# x  = Graph(3,[Node(5,[Node(3)])])       # Syntax similar to functional programming
 # alternatively can use a dict mapping for denoting relationships on a graph
 pdb.set_trace()
 def graph_search (problem, fringe):
