047d0dbcad34dee6689b95513d6b38a23ae96e2f
Chinmay <wadgaonc@oregonstate.edu>
2019-04-16 21:23:45 -0700

    Added successor function with checking.

--- BFS, IDFS, A star search/assignment1.py
+++ BFS, IDFS, A star search/assignment1.py
@@ -6,8 +6,8 @@ class Node:
     def __init__(self,val, child = [], leftbank = [], rightbank = []):     # Node("",[Node(4),Node(5)])
         self.val = val
         self.child = child
-        self.leftbank = []      # C, W, B
-        self.rightbank = []     # C, W, B
+        self.leftbank = leftbank      # C, W, B
+        self.rightbank = rightbank    # C, W, B
 
 class Graph:
     def __init__(self, val, children):
@@ -22,19 +22,144 @@ class Graph:
 sys.argv = [sys.argv[0], 'start1.txt']
 start_file = open(sys.argv[1], "r")
 x = start_file.readline()
-leftbank = []
-leftbank.append(x[0])
-leftbank.append(x[2])
-leftbank.append(x[4])
+leftstart = []
+leftstart.append(x[0])
+leftstart.append(x[2])
+leftstart.append(x[4])
 x = start_file.readline()
-rightbank = []
-rightbank.append(x[0])
-rightbank.append(x[2])
-rightbank.append(x[4])
+rightstart = []
+rightstart.append(x[0])
+rightstart.append(x[2])
+rightstart.append(x[4])
+
+# Reading Files for Goal State
+sys.argv = [sys.argv[0], 'goal1.txt']
+goal_file = open(sys.argv[1], "r")
+x = goal_file.readline()
+leftgoal = []
+leftgoal.append(x[0])
+leftgoal.append(x[2])
+leftgoal.append(x[4])
+x = goal_file.readline()
+rightgoal = []  
+rightgoal.append(x[0])
+rightgoal.append(x[2])
+rightgoal.append(x[4])
+
 
 # x  = Graph(3,[Node(5,[Node(3)])])       # Syntax similar to functional programming
 # alternatively can use a dict mapping for denoting relationships on a graph
 pdb.set_trace()
-def graph_search (problem, fringe):
+
+def succ(state):
+    initial_left = state.leftbank
+    left_chick = initial_left[0]
+    left_wolf = initial_left[1]
+    left_boat = initial_left[2]
+
+    initial_right = state.rightbank
+    right_chick = initial_right[0]
+    right_wolf = initial_right[1]
+    right_boat = initial_right[2]
+
+    # Temp arrays for successors banks
+    succleftbank = initial_left
+    succrightbank = initial_right
+
+    # Temp array for set of successor states
+    succset = []
+
+    # Where is the boat?
+    if (left_boat == 1):
+        if ((left_wolf <= left_chick - 1) and (right_wolf <= right_chick + 1)):     # Checking constraint
+            # Put one chicken in the boat
+            succleftbank = initial_left
+            succrightbank = initial_right
+            succleftbank[0] -= 1
+            succrightbank[0] += 1
+            # Create state(node) and add to set of returned states
+            succset.append(Node(state.val + 1, [], succleftbank, succrightbank))
+        if ((left_wolf <= left_chick - 2) and (right_wolf <= right_chick + 2)): 
+            # Put two chickens in the boat
+            succleftbank = initial_left
+            succrightbank = initial_right
+            succleftbank[0] -= 2
+            succrightbank[0] += 2
+            # Create state(node) and add to set of returned states
+            succset.append(Node(state.val + 1, [], succleftbank, succrightbank))
+        if ((left_wolf - 1 <= left_chick) and (right_wolf + 1 <= right_chick)): 
+            # Put one wolf in the boat
+            succleftbank = initial_left
+            succrightbank = initial_right
+            succleftbank[1] -= 1
+            succrightbank[1] += 1
+            # Create state(node) and add to set of returned states
+            succset.append(Node(state.val + 1, [], succleftbank, succrightbank))
+        if ((left_wolf - 1 <= left_chick - 1) and (right_wolf + 1 <= right_chick + 1)):
+            # Put one wolf and one chicken in the boat
+            succleftbank = initial_left
+            succrightbank = initial_right
+            succleftbank[0] -= 1
+            succrightbank[0] += 1
+            succleftbank[1] -= 1
+            succrightbank[1] += 1
+            # Create state(node) and add to set of returned states
+            succset.append(Node(state.val + 1, [], succleftbank, succrightbank))
+        if ((left_wolf - 2 <= left_chick) and (right_wolf + 2 <= right_chick)):
+            # Put two wolves in the boat
+            succleftbank = initial_left
+            succrightbank = initial_right
+            succleftbank[1] -= 2
+            succrightbank[1] += 2
+            # Create state(node) and add to set of returned states
+            succset.append(Node(state.val + 1, [], succleftbank, succrightbank))
+    elif (right_boat == 1):
+        if ((right_wolf <= right_chick - 1) and (left_wolf <= left_chick + 1)):     # Checking constraint
+            # Put one chicken in the boat
+            succrightbank = initial_right
+            succleftbank = initial_left
+            succrightbank[0] -= 1
+            succleftbank[0] += 1
+            # Create state(node) and add to set of returned states
+            succset.append(Node(state.val + 1, [], succleftbank, succrightbank))
+        if ((right_wolf <= right_chick - 2) and (left_wolf <= left_chick + 2)): 
+            # Put two chickens in the boat
+            succrightbank = initial_right
+            succleftbank = initial_left
+            succrightbank[0] -= 2
+            succleftbank[0] += 2
+            # Create state(node) and add to set of returned states
+            succset.append(Node(state.val + 1, [], succleftbank, succrightbank))
+        if ((right_wolf - 1 <= right_chick) and (left_wolf + 1 <= left_chick)): 
+            # Put one wolf in the boat
+            succrightbank = initial_right
+            succleftbank = initial_left
+            succrightbank[1] -= 1
+            succleftbank[1] += 1
+            # Create state(node) and add to set of returned states
+            succset.append(Node(state.val + 1, [], succleftbank, succrightbank))
+        if ((right_wolf - 1 <= right_chick - 1) and (left_wolf + 1 <= left_chick + 1)):
+            # Put one wolf and one chicken in the boat
+            succrightbank = initial_right
+            succleftbank = initial_left
+            succrightbank[0] -= 1
+            succleftbank[0] += 1
+            succrightbank[1] -= 1
+            succleftbank[1] += 1
+            # Create state(node) and add to set of returned states
+            succset.append(Node(state.val + 1, [], succleftbank, succrightbank))
+        if ((right_wolf - 2 <= right_chick) and (left_wolf + 2 <= left_chick)):
+            # Put two wolves in the boat
+            succrightbank = initial_right
+            succleftbank = initial_left
+            succrightbank[1] -= 2
+            succleftbank[1] += 2
+            # Create state(node) and add to set of returned states
+            succset.append(Node(state.val + 1, [], succleftbank, succrightbank))
+    return succset
+
+
+def bfs_graph_search (problem, fringe):
     closed = []
     fringe = []     # Is kinda like the fifo queue
+    
